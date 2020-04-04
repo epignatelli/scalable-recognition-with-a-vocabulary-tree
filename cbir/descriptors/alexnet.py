d769cd925f7275629799a35bfe091b9f6635aefc
epignatelli <edu.pignatelli@gmail.com>
2020-04-04 10:25:10 +0100

    alexnet gpu

--- cbir/descriptors/alexnet.py
+++ cbir/descriptors/alexnet.py
@@ -6,13 +6,16 @@ from .descriptor_base import DescriptorBase
 class AlexNet(DescriptorBase):
     def __init__(self):
         super(AlexNet, self).__init__("data")
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self._device = torch.device(device)
+
         self.alexnet = torchvision.models.alexnet(pretrained=True)
+        self.alexnet.to(self._device)
         self.alexnet.eval()
 
     def describe(self, cv_image):
         image = torch.as_tensor(cv_image, dtype=torch.float32) / 255
-        image = image.permute(2, 1, 0)
-        image = torch.as_tensor(image).unsqueeze(0)
+        image = image.permute(2, 1, 0).unsqueeze(0).to(self._device)
         with torch.no_grad():
             all_fetures = self.alexnet.features(image).detach()
             featurest_list = all_fetures.permute(0, 2, 3, 1).contiguous().view(-1, 256)
--- cbir/encoders/alexnet.py
+++ cbir/encoders/alexnet.py
@@ -4,12 +4,15 @@ import torchvision
 
 class AlexNet(object):
     def __init__(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self._device = torch.device(device)
+
         self.alexnet = torchvision.models.alexnet(pretrained=True)
+        self.alexnet.to(self._device)
         self.alexnet.eval()
 
     def embedding(self, cv_image):
         image = torch.as_tensor(cv_image, dtype=torch.float32) / 255
-        image = image.permute(2, 1, 0)
-        image = torch.as_tensor(image).unsqueeze(0)
+        image = image.permute(2, 1, 0).unsqueeze(0).to(self._device)
         with torch.no_grad():
             return self.alexnet(image).detach().squeeze().cpu().numpy()
